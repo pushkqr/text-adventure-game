ac0bce9b6ee7fd13368565c6fdb4fd0e9b1f29fe
pushkqr <pushkaroli61@gmail.com>
2022-07-11 18:23:10 +0530

    add

--- checkUser.c
+++ checkUser.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "checkUser.h"
 
-int checkUser(int *pUserPos, char *consoleInput, const int relicPos)
+int checkUser(int *pUserPos, const int relicPos, int monster1Pos, int monster2Pos)
 {
 
 	if(*pUserPos == relicPos)
@@ -10,9 +10,12 @@ int checkUser(int *pUserPos, char *consoleInput, const int relicPos)
 	    	return 1;
 		//printf("Rush to the exit before the Relic Guardian catches you!\n");
 	}
+	else if(*pUserPos == monster1Pos || *pUserPos == monster2Pos)
+	{
+		return 1;
+	}
 	else
 	{
 		return 0;
 	}
-
 }
