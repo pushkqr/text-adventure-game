4ab9d50c46c73f11824c1e85e0327660e425250f
Pushkar Oli <108482093+pushkqr@users.noreply.github.com>
2022-08-02 20:06:47 +0530

    reformat code

--- main.c
+++ main.c
@@ -4,26 +4,37 @@
 #include <ctype.h>
 #include <time.h>
 
-////
+//////////////////////////////////////////////////////////////////////////
+//Function Prototypes
+//////////////////////////////////////////////////////////////////////////
 _Bool getInput(void);
-int action(int *room_curr_idx, int *dir_idx);
-int isInput(int *dir_idx);
+int action();
 int list_organise();
 void delay(int sec);
-void pickup(int *room_curr_idx, int *dir_idx);
-void drop(int *room_curr_idx, int *dir_idx);
+void item_pickup();
+void item_drop();
+void buffer_command();
 
-
-////
+//////////////////////////////////////////////////////////////////////////
+// Global variables
+//////////////////////////////////////////////////////////////////////////
 char buffer_input[20];
+int room_curr_idx;
+enum list_commands{ north, south, east, west, quit, help, search,  pickup , drop, inventory, invalid};
+enum list_commands curr_command;
 
-////
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
 struct item_info
 {
     char name[40];
     int hp;
 };
 
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
 struct player
 {
     char name[20];
@@ -31,6 +42,9 @@ struct player
     struct item_info inventory[5];
 }p1;
 
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
 struct map
 {
     char desc[40];
@@ -38,7 +52,10 @@ struct map
     int move[4];
 }room[4];
 
-////
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+
 int main()
 {
     printf("\n      Welcome to CaveExplorer        \n");
@@ -48,24 +65,15 @@ int main()
 
     printf("\nHINT:Try using the \"help\" action.\n");
 
-    int room_curr_idx = 0;
-    int dir_idx = 0;
-
-    for(int i = 0; i<4; i++)
-    {
-        strcpy(room[i].desc, "You're in a big dark cave-like hall.\n");
-        strcpy(p1.inventory[i].name,"");
-    }
-    strcpy(p1.inventory[4].name,"");
-
     ////room1
     strcpy(room[0].item[0].name ,"Rock");
     strcpy(room[0].item[1].name ,"Cloth Rags");
     strcpy(room[0].item[2].name ,"Stick");
-    room[0].move[0] =-1;
-    room[0].move[1] =1;
-    room[0].move[2] =-1;
-    room[0].move[3] =-1;
+    room[0].move[0] = -1;
+    room[0].move[1] = 1;
+    room[0].move[2] = -1;
+    room[0].move[3] = -1;
+
     ////room2
     strcpy(room[1].item[0].name ,"Necklace");
     strcpy(room[1].item[1].name ,"Rusted Pole");
@@ -74,6 +82,7 @@ int main()
     room[1].move[1] =-1;
     room[1].move[2] =2;
     room[1].move[3] =3;
+
     ////room3
     strcpy(room[2].item[0].name ,"Rock");
     strcpy(room[2].item[1].name ,"Skull");
@@ -82,6 +91,7 @@ int main()
     room[2].move[1] =-1;
     room[2].move[2] =-1;
     room[2].move[3] =1;
+
     ////room4
     strcpy(room[3].item[0].name ,"Rock");
     strcpy(room[3].item[1].name ,"Jacket");
@@ -91,21 +101,38 @@ int main()
     room[3].move[2] =1;
     room[3].move[3] =-1;
 
+    for(int i = 0; i<4; i++)
+    {
+        strcpy(room[i].desc, "You're in a big dark cave-like hall.\n");
+        strcpy(p1.inventory[i].name,"");
+    }
+
+    for(int i = 0; i<5; i++)
+    {
+        strcpy(p1.inventory[i].name,"");
+    }
+
     while(getInput() == 1)
     {
         list_organise();
+        buffer_command();
 
-        if(action(&room_curr_idx,&dir_idx) == 0)
+        if(action() == 0)
            break;
+
+        printf("\nYou're now in room \"%d\"\n", (room_curr_idx)+1);
 
     }
 
     return 0;
 }
 
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
 _Bool getInput(void)
 {
-    char *pBUF = buffer_input;
+    char *p = buffer_input;
 
     strcpy(buffer_input,"");
 
@@ -114,38 +141,43 @@ _Bool getInput(void)
 
     for(int i = 0; i<strlen(buffer_input); i++)
     {
-        *(pBUF+i) = tolower( *(pBUF+i) );
+        *(p+i) = tolower( *(p+i) );
     }
 
     return (1);
 }
 
-int action(int *room_curr_idx, int *dir_idx)
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+
+
+int action()
 {
     if(strcmp(buffer_input,"")==0 || buffer_input == NULL )
     {
         return 1;
     }
 
-    else if(strcmp(buffer_input,"quit")==0 )
+    else if(curr_command == 4 )
     {
         printf("\n  HASTA LA VISTA ! \n");
         return 0;
     }
 
-    else if(strcmp(buffer_input,"pickup")==0 )
+    else if(curr_command == 7)
     {
-        pickup(room_curr_idx, dir_idx);
+        item_pickup();
         return 1;
     }
 
-    else if(strcmp(buffer_input,"drop")==0 )
+    else if(curr_command == 8)
     {
-        drop(room_curr_idx, dir_idx);
+        item_drop();
         return 1;
     }
 
-    else if(strcmp(buffer_input,"inventory")==0 )
+    else if(curr_command == 9)
     {
         for(int j=0; j<5;j++)
         {
@@ -154,7 +186,7 @@ int action(int *room_curr_idx, int *dir_idx)
         return 1;
     }
 
-    else if(strcmp(buffer_input,"help")==0 )
+    else if(curr_command == 5)
     {
         printf("\nSearch the rooms for your desired treasure but beware of the UNKNOWN!....\n");
         printf("\n\"north\" : moves in the north direction");
@@ -163,14 +195,14 @@ int action(int *room_curr_idx, int *dir_idx)
         printf("\n\"west\" : moves in the west direction");
         printf("\n\"search\" : searches the room");
         printf("\n\"pickup\" : opens the pickup menu");
-        printf("\n\"drop\" : opens the pickup menu\n");
+        printf("\n\"drop\" : opens the drop menu\n");
 
         return 1;
     }
 
-    else if(strcmp(buffer_input,"search")==0)
+    else if(curr_command == 6)
     {
-        printf("\n%s\n", room[*room_curr_idx].desc);
+        printf("\n%s\n", room[room_curr_idx].desc);
 
         for(int i = 0; i < 1; i++)
         {
@@ -185,16 +217,15 @@ int action(int *room_curr_idx, int *dir_idx)
 
         for(int i = 0; i< 3; i++)
         {
-            printf("\n %s\n", room[*room_curr_idx].item[i].name);
+            printf("\n %s\n", room[room_curr_idx].item[i].name);
         }
     }
 
-    else if(isInput(dir_idx) == 1 )
+    else if(curr_command >= 0 && curr_command < 4)
     {
-        if(room[*room_curr_idx].move[*dir_idx] != (-1))
+        if(room[room_curr_idx].move[curr_command] != (-1))
         {
-            *room_curr_idx = room[*room_curr_idx].move[*dir_idx];
-            printf("\nYou're now in room \"%d\"\n", (*room_curr_idx)+1);
+            room_curr_idx = room[room_curr_idx].move[curr_command];
             return 1;
         }
         else
@@ -212,34 +243,10 @@ int action(int *room_curr_idx, int *dir_idx)
 
 }
 
-int isInput(int *dir_idx)
-{
-    const int NORTH_INDEX = 0;
-    const int SOUTH_INDEX = 1;
-    const int EAST_INDEX = 2;
-    const int WEST_INDEX = 3;
-
-    if(strcmp(buffer_input,"north")==0 )
-    {
-        *dir_idx = NORTH_INDEX;
-    }
-    else if(strcmp(buffer_input,"south")==0 )
-    {
-        *dir_idx = SOUTH_INDEX;
-    }
-    else if(strcmp(buffer_input,"east")==0 )
-    {
-        *dir_idx = EAST_INDEX;
-    }
-    else if(strcmp(buffer_input,"west")==0 )
-    {
-        *dir_idx = WEST_INDEX;
-    }
-    return 1;
-}
-
-
-void pickup(int *room_curr_idx, int *dir_idx)
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+void item_pickup()
 {
     int i = 0;
     int item_idx;
@@ -252,7 +259,7 @@ void pickup(int *room_curr_idx, int *dir_idx)
 
     for(i = 0; i< 3; i++)
     {
-        printf("\n%d.%s\n", i+1,room[*room_curr_idx].item[i].name);
+        printf("\n%d.%s\n", i+1,room[room_curr_idx].item[i].name);
     }
 
     printf("\nType \"0\" to stop picking up items.\n");
@@ -268,7 +275,7 @@ void pickup(int *room_curr_idx, int *dir_idx)
         {
             break;
         }
-        else if(strcmp(room[*room_curr_idx].item[item_idx].name,"") == 0)
+        else if(strcmp(room[room_curr_idx].item[item_idx].name,"") == 0)
         {
             printf("\nNothing is there to pick up.\n");
             continue;
@@ -285,11 +292,11 @@ void pickup(int *room_curr_idx, int *dir_idx)
             {
                 if(strcmp(p1.inventory[i].name,"") == 0 )
                 {
-                    strcpy(p1.inventory[i].name,room[*room_curr_idx].item[item_idx].name);
+                    strcpy(p1.inventory[i].name,room[room_curr_idx].item[item_idx].name);
 
                     printf("\nPicked up %s.\n", p1.inventory[i].name);
 
-                    strcpy(room[*room_curr_idx].item[item_idx].name,"");
+                    strcpy(room[room_curr_idx].item[item_idx].name,"");
 
                     break;
                 }
@@ -297,16 +304,82 @@ void pickup(int *room_curr_idx, int *dir_idx)
 
             if(strcmp(p1.inventory[4].name,"") == 1)
             {
-                printf("Inventory is full.");
+                printf("\nInventory is full.\n");
                 break;
             }
 
-            continue;
         }
     }
 
 }
 
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+void item_drop()
+{
+    int i = 0;
+    int item_idx;
+
+    for(i = 0; i< 5; i++)
+    {
+        printf("\n%d.%s\n", i+1,p1.inventory[i].name);
+    }
+
+    printf("\nType \"0\" to stop dropping items.\n");
+
+    while(1)
+    {
+        printf("\nWhich item(s) do you want to drop:");
+        scanf("%d", &item_idx);
+
+        item_idx -= 1;
+
+        if(item_idx == (-1))
+        {
+            break;
+        }
+        else if(strcmp(p1.inventory[item_idx].name,"") == 0)
+        {
+            printf("\nNothing is there to drop .\n");
+            continue;
+        }
+        else if(item_idx > 4 || item_idx < 0)
+        {
+            printf("\nPlease select from the given items to drop.\n");
+            continue;
+        }
+
+        else
+        {
+            for(i = 0; i<5; i++)
+            {
+                if(strcmp(room[room_curr_idx].item[i].name,"") == 0)
+                {
+                    strcpy(room[room_curr_idx].item[i].name,p1.inventory[item_idx].name);
+
+                    printf("\nDropped %s.\n", room[room_curr_idx].item[i].name);
+
+                    strcpy(p1.inventory[item_idx].name,"");
+
+                    break;
+                }
+
+            }
+            if(strcmp(room[room_curr_idx].item[2].name,"") != 0 || strcmp(room[room_curr_idx].item[2].name,"") != 0)
+            {
+                printf("\nCannot drop more items in this room.\n");
+                break;
+            }
+
+        }
+    }
+
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
 int list_organise()
 {
     int i = 0;
@@ -341,67 +414,9 @@ int list_organise()
     return 0;
 }
 
-void drop(int *room_curr_idx, int *dir_idx)
-{
-    int i = 0;
-    int item_idx;
-
-    for(i = 0; i< 5; i++)
-    {
-        printf("\n%d.%s\n", i+1,p1.inventory[i].name);
-    }
-
-    printf("\nType \"0\" to stop dropping up items.\n");
-
-    while(1)
-    {
-        printf("\nWhich item(s) do you want to drop:");
-        scanf("%d", &item_idx);
-
-        item_idx -= 1;
-
-        if(item_idx == (-1))
-        {
-            break;
-        }
-        else if(strcmp(p1.inventory[item_idx].name,"") == 0)
-        {
-            printf("\nNothing is there to drop .\n");
-            continue;
-        }
-        else if(item_idx > 4 || item_idx < 0)
-        {
-            printf("Please select from the given items to drop.\n");
-            continue;
-        }
-
-        else
-        {
-            for(i = 0; i<5; i++)
-            {
-                if(strcmp(room[*room_curr_idx].item[i].name,"") == 0)
-                {
-                    strcpy(room[*room_curr_idx].item[i].name,p1.inventory[item_idx].name);
-
-                    printf("\nDropped %s.\n", room[*room_curr_idx].item[i].name);
-
-                    strcpy(p1.inventory[item_idx].name,"");
-
-                    break;
-                }
-
-            }
-            if(strcmp(room[*room_curr_idx].item[2].name,"") != 0 || strcmp(room[*room_curr_idx].item[2].name,"") != 0)
-            {
-                printf("Cannot drop more items in this room.");
-                break;
-            }
-
-        }
-    }
-
-}
-
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
 void delay(int sec)
 {
     int milliSec = 1000 * sec;
@@ -412,3 +427,31 @@ void delay(int sec)
         ;
 }
 
+//////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////
+void buffer_command()
+{
+    if(strcmp(buffer_input,"north")==0)
+        curr_command = north;
+    else if(strcmp(buffer_input,"south")==0)
+        curr_command = south;
+    else if(strcmp(buffer_input,"east")==0)
+        curr_command = east;
+    else if(strcmp(buffer_input,"west")==0)
+        curr_command = west;
+    else if(strcmp(buffer_input,"quit")==0)
+        curr_command = quit;
+    else if(strcmp(buffer_input,"help")==0)
+        curr_command = help;
+    else if(strcmp(buffer_input,"search")==0)
+        curr_command = search;
+    else if(strcmp(buffer_input,"pickup")==0)
+        curr_command = pickup;
+    else if(strcmp(buffer_input,"drop")==0)
+        curr_command = drop;
+    else if(strcmp(buffer_input,"inventory")==0)
+        curr_command = inventory;
+    else
+        curr_command = invalid;
+}
